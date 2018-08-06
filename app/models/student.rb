216acbc9150269fa4011112ee95319db86786971
Elaine Anderson <eparie@bu.edu>
2018-08-06 19:54:49 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -17,15 +17,15 @@ class Student < ActiveRecord::Base
   def self.search(search_name)
     search_name = search_name.downcase
     array = []
-    if search_name != ''
-      self.all.each do |student|
-      array << student.name.downcase.include?(search_name)
-      array
-    end
+      if search_name != ''
+        self.all.each do |student|
+          array << student.name.downcase.include?(search_name)
+          array
+        end
       else
         self.all
+      end
     end
-  end
 
 
 
