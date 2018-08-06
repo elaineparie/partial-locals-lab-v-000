01a177723cfe5800dc5229d69a37188ed0be7b75
Elaine Anderson <eparie@bu.edu>
2018-08-06 19:36:33 +0000

    automatically backed up by learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,21 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(student_name)
+    student_name = student_name.downcase
+    array = []
+    if student_name == ''
+      self.all
+    else
+      self.all.collect do |student|
+      if student.name.downcase.include?(student_name)
+      array << student
+      array
+    end
+    end
+  end
+    end
+
+
 end
